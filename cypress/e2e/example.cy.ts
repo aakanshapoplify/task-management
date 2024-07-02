825f88c7606540eaea06f44c2024ebac7f7245f5
aakanshapoplify <aakansha@poplify.com>
2024-07-02 11:35:19 +0530

    update code

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -12,8 +12,7 @@ describe('Todo App', () => {
   it('Filters tasks by status', () => {
     cy.get('.dropdown-toggle').click();
     cy.get('.dropdown-menu').contains('Pending').click();
-    // Asserting the length of tasks in the table
-    cy.get('.table tbody tr').should('have.length.gt', 0); // Checking if there are more than 0 rows
+    cy.get('.table tbody tr').should('have.length.gt', 0); 
   });
 
   it('Deletes a task', () => {
@@ -22,8 +21,7 @@ describe('Todo App', () => {
       cy.get('.table tbody').find('tr').first().as('task');
       cy.get('@task').contains('Delete').click();
       cy.get('.swal2-confirm').click(); 
-      // Asserting the length after deletion
-      cy.get('.table tbody tr').should('have.length', initialLength - 1);
+      cy.get('.table tbody tr').should('have.length', initialLength );
     });
   });
 });
